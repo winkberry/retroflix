1a4922c22928fafe5f9187fe8afaae5600ee05cf
sahayana <saokhueyang@gmail.com>
2022-02-04 21:38:31 +0900

    리뷰관련  통계 정보 및 차트 코드만 생성

--- review/views.py
+++ review/views.py
@@ -2,27 +2,26 @@ from django.shortcuts import render, get_object_or_404, redirect
 from movie.models import Movie
 from django.db.models import Count, Avg
 from django.contrib.auth.decorators import login_required
-# from .forms import ReviewForm
+from .forms import ReviewForm
 from review.models import Review, cal_age
 
 # Create your views here.
 
 
 
-# 이 부분은 detail페이지 구현하는 부분에서 context 만들 때 참고하면 좋을 것 같습니다. (실제 Review앱에서 detail페이지 구현하지않음)
+
 def movie_detail(request, movie_id):
     movie = get_object_or_404(Movie, id=movie_id)
     total_user = movie.reviews.all()
     total_user_count = total_user.count()
-    
+    male_user_count = total_user.filter(customuser__gender__ieaxct='male').count()
 
     # 리뷰 성별 비율
-    male_user_count = total_user.filter(customuser__gender__ieaxct='male').count()
     male_gender_rate = (male_user_count/total_user_count) * 100
     female_gender_rate = 100 - male_gender_rate
-    
-    # 리뷰 연령별 비율  
+    gender_rate = [male_gender_rate, female_gender_rate]
 
+    # 리뷰 연령별 비율  
     # user_age = list(map(lambda x : cal_age(x.author.birthday), total_user))
     user_age = [cal_age(review.author.birthday) for review in total_user]
     gen_10 = gen_20 = gen_30 = gen_40 = 0
@@ -35,21 +34,18 @@ def movie_detail(request, movie_id):
         elif age >= 20:
             gen_20 += 1
         else:
-            gen_10 += 1       
+            gen_10 += 1    
 
+    generation_count = [gen_10, gen_20, gen_30, gen_40]
+    generation_rate = [(gen_cnt / total_user_count) * 100 for gen_cnt in generation_count]
 
     context = {
         'movie': movie,
-        'male_gender_rate':male_gender_rate,
-        'female_gender_rate': female_gender_rate,
-        'generation_10_rate': (gen_10 / total_user) * 100,
-        'generation_20_rate': (gen_20 / total_user) * 100,
-        'generation_30_rate': (gen_30 / total_user) * 100,
-        'generation_40_rate': (gen_40 / total_user) * 100,        
+        'gender_rate': gender_rate,
+        'generation_rate' : generation_rate      
     }
 
-    return render('request', 'main/detail.html', context)
-
+    return render(request, 'main/detail.html', context)
 
 
 
@@ -65,7 +61,6 @@ def review_create(request, movie_id):
         movie.star = movie.reviews.all().aggregate(Avg('star')).get('star__avg')
         movie.save()
         return redirect('main')
-
 
 
 # @login_required
