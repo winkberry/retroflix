21bee3a75b7775aca49cea442b876f7753610b19
sahayana <saokhueyang@gmail.com>
2022-02-07 15:51:07 +0900

    디테일 페이지 생성 및 리뷰시스템 개선

--- review/urls.py
+++ review/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = 'review'
 
 urlpatterns = [
-   path('detail/<int:movie_id>/reviews/create/', views.review_create, name='review_create'),
+   # path('detail/<int:movie_id>/reviews/create/', views.review_create, name='review_create'),
    # path('detail/<int:movie_id>/', views.review_list, name='review_list')
+   path('moviedetail/<int:movie_id>/reviews/create/', views.review_create, name='review_create'),
+   path('moviedetail/reviews/<int:review_id>/delete/', views.review_delete, name='review_delete'),
+
 ]
